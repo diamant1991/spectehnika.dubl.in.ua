2ac1de22d495e8e6cd75145a28c5c434607506f0
diamant1991 <diamant1988@mail.ru>
2015-01-30 16:12:52 +0200

    add files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+  var mySwiper = $('.swiper-container').swiper({
+    pagination: '.pagination',
+    loop:true,
+    grabCursor: true,
+    paginationClickable: true,
+    slidesPerView: 4,
+  });
